10004110c299cc2f409faf267544ae2a28dda45d
gian <gianmarco.clu23@gmail.com>
2024-02-24 15:21:11 +0100

    first draft of navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ContextProvider } from "../ContextProvider";
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/cart">Cart</Link></li>
+      </ul>
+    </nav>
       <ContextProvider>
         <Component {...pageProps} />
       </ContextProvider>
