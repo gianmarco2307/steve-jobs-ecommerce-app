236807067ad218962d57fc333c90a1b70ddf7c7b
gian <gianmarco.clu23@gmail.com>
2024-02-24 13:05:33 +0100

    Updated ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,31 +7,69 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import { Product } from "@/declarations";
+import { Button, TextField } from "@mui/material";
+import { ShoppingCartRounded } from "@mui/icons-material";
+import { AppContext } from "@/ContextProvider";
 
+export default function ProductCard({ product }: { product: Product["id"] }) {
+  const [quantity, setQuantity] = React.useState<number | null>(null);
 
-export default function ProductCard({ product }: { product: Product }) {
+  const { products, addToCart } = React.useContext(AppContext);
+  const productToShow: Product | undefined = products?.find(
+    (el) => el.id === product
+  );
+
+  const onChangeQuantity = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantity(parseInt(e.target.value));
+  };
+
+  if (!productToShow) {
+    return null;
+  }
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        avatar={
-          <Avatar src={product.thumbnail} />
-        }
-        title={product.title}
-        subheader={`Disponibili: ${product.qty}`}
+        avatar={<Avatar src={productToShow?.thumbnail} />}
+        title={productToShow?.title}
+        subheader={`Disponibili: ${productToShow?.qty}`}
       />
       <CardMedia
         component="img"
         height="194"
-        image={product.image}
+        image={productToShow?.image}
         alt="Image of the product"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          {product.description}
+          {productToShow?.description}
+        </Typography>
+        <br />
+        <Typography gutterBottom variant="h6" component="div">
+          {productToShow?.price.toFixed(2)} €
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
+        <TextField
+          id="standard-number"
+          label="Quantità"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="standard"
+          inputProps={{ min: 0, max: productToShow?.qty }}
+          value={quantity}
+          onChange={onChangeQuantity}
+        />
+        <Button
+          variant="outlined"
+          startIcon={<ShoppingCartRounded />}
+          disabled={!quantity}
+          onClick={() => addToCart(productToShow)}
+        >
+          Add item
+        </Button>
       </CardActions>
     </Card>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import { AppContext } from "@/ContextProvider";
 import ProductCard from "@/components/ProductCard";
-import RecipeReviewCard from "@/components/ProductCard";
+import { styled } from "@mui/material";
 import { useContext } from "react";
+
+const CardDiv = styled("div")({
+  display: "flex",
+  flexFlow: "row wrap",
+  justifyContent: "space-around",
+  "& > *": {
+    marginBottom: "20px",
+  },
+});
 
 export default function Home() {
   const { products } = useContext(AppContext);
 
   return (<>
   <h1>Home</h1>
-  <div>
+  <CardDiv>
     {products?.map((product) => (
-      <ProductCard key={product.id} product={product}/>
+      <ProductCard key={product.id} product={product.id}/>
     ))}
-    </div>
+    </CardDiv>
   </>);
 }
